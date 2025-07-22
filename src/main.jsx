9baa0ccfecbfa41d7f0247d08e93a4a9c723f136
Abdullah <abdullahmubashir878@gmail.com>
2025-07-22 21:51:41 +0500

    Remove .env file from repo

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-=======
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import DoctorContextProvider from "./context/DoctorContext.jsx";
 import AdminContextProvider from "./context/AdminContext.jsx";
->>>>>>> 66cd174411b056e40124bd25c8db42fc65194bfb
 import AppContextProvider from "./context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-<<<<<<< HEAD
     <AppContextProvider>
       <App />
     </AppContextProvider>
-=======
     <AdminContextProvider>
       <DoctorContextProvider>
         <AppContextProvider>
@@ -27,6 +22,5 @@ createRoot(document.getElementById("root")).render(
         </AppContextProvider>
       </DoctorContextProvider>
     </AdminContextProvider>
->>>>>>> 66cd174411b056e40124bd25c8db42fc65194bfb
   </BrowserRouter>
 );
